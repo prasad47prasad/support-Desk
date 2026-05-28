9ba11983e1e74be0f21ff7c59e4fa2f9484031b8
Prasad Varma <gadidurga6@gmail.com>
2026-05-28 13:18:32 +0530

    Fix database config and add gitignore

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:admin123@localhost/support_ticket_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable is missing")
 
 engine = create_engine(
     DATABASE_URL,
